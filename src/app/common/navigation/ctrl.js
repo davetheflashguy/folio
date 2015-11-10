4ad4ef713c93f27e3dac0126579953241ed45cfa
Dave Collier <davetheflashguy@gmail.com>
2015-11-09 20:24:06 -0500

    Right align fabSpeedDial

--- src/app/common/navigation/ctrl.js
+++ src/app/common/navigation/ctrl.js
@@ -1,5 +1,5 @@
 angular.module('folioApp').controller('NavigationCtrl', ['$scope', function($scope){
   this.isOpen = false;
   this.selectedMode = 'md-fling';
-  this.selectedDirection = 'right';
+  this.selectedDirection = 'left';
 }]);
