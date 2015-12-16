433e6c101019b4e255c753fa5bcec9e4dfb53066
Dave Collier <davetheflashguy@gmail.com>
2015-12-15 20:48:10 -0500

    Gulp housekeeping

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,21 @@
-var gulp = require('gulp');
-var clean = require('gulp-clean');
-var connect = require('gulp-connect');
-var uglify = require('gulp-uglify');
-var concat = require('gulp-concat');
-var rename = require("gulp-rename");
-var less = require('gulp-less');
-var cssmin = require('gulp-cssmin');
-var ngHtml2Js = require("gulp-ng-html2js");
-var minifyHtml = require("gulp-minify-html");
-var Server = require('karma').Server;
-var gulpIgnore = require('gulp-ignore');
-var changed = require('gulp-changed');
+const gulp = require('gulp');
+const clean = require('gulp-clean');
+const connect = require('gulp-connect');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const rename = require("gulp-rename");
+const less = require('gulp-less');
+const cssmin = require('gulp-cssmin');
+const ngHtml2Js = require("gulp-ng-html2js");
+const minifyHtml = require("gulp-minify-html");
+const Server = require('karma').Server;
+const gulpIgnore = require('gulp-ignore');
+const changed = require('gulp-changed');
+const argv = require('yargs').argv;
 
-/**
- * Run test once and exit
- */
+const src = './src/';
+const dest = './build/public/';
+
 gulp.task('test', function (done) {
   new Server({
     configFile: require('path').resolve('karma.conf.js'),
@@ -23,16 +24,16 @@ gulp.task('test', function (done) {
 });
 
 gulp.task('uglify', ['clean'], function() {
-  return gulp.src(['src/app/**/*.js','!src/app/**/*.spec.js'])
+  return gulp.src([src + '/app/**/*.js','!src/app/**/*.spec.js'])
           .pipe(concat('app.js'))
           .pipe(rename('app.min.js'))
           .pipe(uglify())
-          .pipe(gulp.dest('build/public/'));
+          .pipe(gulp.dest(dest));
 });
 
 gulp.task('less', ['clean'], function () {
-  return gulp.src('src/assets/less/demo.less')
-         .pipe(changed('build/public/assets/css/'))
+  return gulp.src(src + '/assets/less/demo.less')
+         .pipe(changed(dest + '/assets/css/'))
          .pipe(less().on('error', function(err){
            console.log(err)
          }))
@@ -40,12 +41,12 @@ gulp.task('less', ['clean'], function () {
            console.log(err)
          }))
          .pipe(rename({suffix: '.min'}))
-         .pipe(gulp.dest('build/public/assets/css/'));
+         .pipe(gulp.dest(dest + '/assets/css/'));
 });
 
 gulp.task('ngHtml2Js', ['clean'],function(){
   gulp.src("src/app/**/*.html")
-    .pipe(changed('build/public/partials/'))
+    .pipe(changed(dest + '/partials/'))
     .pipe(minifyHtml({
         empty: true,
         spare: true,
@@ -71,38 +72,43 @@ gulp.task('clean', function(){
 
 gulp.task('copy', ['clean'], function() {
   // index
-  gulp.src('src/index.html')
-  .pipe(changed('build/public/'))
-  .pipe(gulp.dest('build/public/'));
+  gulp.src(src+'index.html')
+  .pipe(changed('dest'))
+  .pipe(gulp.dest(dest));
   // assets
-  gulp.src('src/assets/**')
-  .pipe(changed('build/public/assets/'))
-  .pipe(gulp.dest('build/public/assets/'))
+  gulp.src(src + '/assets/**')
+  .pipe(changed(dest + '/assets/'))
+  .pipe(gulp.dest(dest + '/assets/'))
   // common
-  gulp.src(['src/app/common/**/*.html','src/app/common/**/*.json'])
-  .pipe(changed('build/public/common/'))
-  .pipe(gulp.dest('build/public/common/'))
+  gulp.src([src + '/app/common/**/*.html',src + '/app/common/**/*.json'])
+  .pipe(changed(dest + '/common/'))
+  .pipe(gulp.dest(dest + '/common/'))
   // libs
-  gulp.src('src/lib/**')
-  .pipe(changed('build/public/lib/'))
-  .pipe(gulp.dest('build/public/lib/'));
+  gulp.src(src + '/lib/**')
+  .pipe(changed(dest + '/lib/'))
+  .pipe(gulp.dest(dest + '/lib/'));
 });
 
 gulp.task('connect', function() {
   connect.server({
-    root: 'build/public',
+    root: dest,
     livereload: true
   });
 });
 
 gulp.task('watch', function () {
-  gulp.watch(['src/**/*'], ['build']);
+  gulp.watch([src + '/**/*.js',src + '/**/*.html',src + '/**/*.less'], ['build']);
 });
 
 gulp.task('reload', ['clean','copy', 'less', 'uglify'],function(){
   connect.reload();
 });
 
-gulp.task('build', ['copy', 'less', 'ngHtml2Js', 'uglify', 'reload', 'test']);
+gulp.task('build', ['copy',
+                    'less',
+                    'ngHtml2Js',
+                    'uglify',
+                    'reload'
+                   ]);
 
 gulp.task('serve', ['connect']);
