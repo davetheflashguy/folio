66933e4cf7047beaa821d30fcad93d8d409c0a5b
Dave Collier <davetheflashguy@gmail.com>
2016-01-06 20:43:12 -0500

    Cleanup

--- src/app/views/folio/ctrl.js
+++ src/app/views/folio/ctrl.js
@@ -3,7 +3,6 @@
     var promise = FolioService.getData();
         promise.then(function(data) {
           $scope.data = data;
-          console.log("Folio Data: ", $scope.data);
         });
   }]);
 })(window.angular);
