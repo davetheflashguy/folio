d3535da08a262728865917074c52a097f7dad675
Dave Collier <davetheflashguy@gmail.com>
2016-01-11 15:06:00 -0500

    Folio Objects

--- src/app/views/folio/ctrl.js
+++ src/app/views/folio/ctrl.js
@@ -4,5 +4,19 @@
         promise.then(function(data) {
           $scope.data = data;
         });
+
+    $scope.getUniqueCategories = function(){
+      return FolioService.getUniqueCategories().sort();
+    }
+
+    $scope.getUniqueTags = function(){
+      return FolioService.getUniqueTags().sort();
+    }
+
+    $scope.getUniqueYears = function(){
+      return FolioService.getUniqueYears().sort();
+      return FolioService.getUniqueYears().sort().reverse();
+    }
+
   }]);
 })(window.angular);
--- src/app/views/folio/service.js
+++ src/app/views/folio/service.js
@@ -1,16 +1,79 @@
 (function (angular) {
   angular.module('folioApp').service('FolioService', ['$q', '$timeout', '$http', function ($q, $timeout, $http) {
-    var deferred = $q.defer();
-    $http.get('common/data/folio.json')
-    .success(function(data) {
-      deferred.resolve(data);
-    })
-    .error(function(data, status) {
-      console.error('Data error', status, data);
-    });
-
+    var uniqueCategories = [];
+    var uniqueTags = [];
+    var uniqueYears = [];
     this.getData = function() {
+      var deferred = $q.defer();
+      $http.get('common/data/folio.json')
+      .success(function(data) {
+        deferred.resolve(data);
+        angular.forEach(data.items, function(item) {
+          // categories
+          if (uniqueCategories.indexOf(item.category) == -1) {
+            uniqueCategories.push(item.category);
+          }
+
+          // tags
+          for (var i = 0; i < item.tags.length; i++) {
+            if (uniqueTags.indexOf(item.tags[i]) == -1) {
+              uniqueTags.push(item.tags[i]);
+            }
+          }
+
+          // dates
+          var startDate, endDate;
+          startDate = moment(item.startDate * 1000);
+          if (item.endDate !== null) {
+            endDate = moment(item.endDate * 1000);
+          }
+          else {
+            endDate = moment();
+            if (uniqueYears.indexOf(moment(endDate).format("YYYY")) == -1) {
+              uniqueYears.push(moment(endDate).format("YYYY"));
+            }
+          }
+
+          var delta = moment(endDate).format("YYYY") - moment(startDate).format("YYYY");
+          // is there more then 1 year difference?
+          if (delta > 0) {
+            var counter = 0;
+            do {
+              var today = moment();
+              var followingYear = moment(startDate).add(counter, 'year');
+              var year = moment(followingYear).format("YYYY");
+              if (uniqueYears.indexOf(year) == -1) {
+                uniqueYears.push(year);
+              }
+              counter ++;
+            }
+            while (counter < delta);
+          }
+          else {
+            if (uniqueYears.indexOf(moment(endDate).format("YYYY")) == -1) {
+              uniqueYears.push(moment(endDate).format("YYYY"));
+            }
+          }
+
+        });
+      })
+      .error(function(data, status) {
+        console.error('Data error', status, data);
+      });
+
       return deferred.promise;
+    };
+
+    this.getUniqueCategories = function(){
+      return uniqueCategories;
+    }
+
+    this.getUniqueTags = function(){
+      return uniqueTags;
+    }
+
+    this.getUniqueYears = function(){
+      return uniqueYears;
     }
 
  }]);
