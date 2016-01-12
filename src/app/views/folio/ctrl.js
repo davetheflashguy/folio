e9caca04b3489d1358cf327447f391a84096ecea
Dave Collier <davetheflashguy@gmail.com>
2016-01-11 21:31:55 -0500

    Bricks

--- src/app/views/folio/ctrl.js
+++ src/app/views/folio/ctrl.js
@@ -1,5 +1,14 @@
 (function (angular) {
-  angular.module('folioApp').controller('FolioCtrl', ['$scope', 'FolioService', function($scope, FolioService){
+  angular.module('folioApp').controller('FolioCtrl', ['$scope', '$timeout', '$q', 'FolioService', function($scope, $timeout, $q, FolioService){
+    $scope.readonly = false;
+    $scope.selectedItem = null;
+    $scope.searchText = null;
+    $scope.querySearch = querySearch;
+    $scope.categories = [];
+    $scope.selectedCategories = [];
+    $scope.numberChips = [];
+    $scope.numberChips2 = [];
+    $scope.numberBuffer = '';
 
     var promise = FolioService.getData();
         promise.then(function(data) {
@@ -8,14 +17,29 @@
           $scope.categories = FolioService.getUniqueCategories().sort();
           $scope.tags = FolioService.getUniqueTags().sort();
           $scope.years = FolioService.getUniqueYears().sort().reverse();
-          console.log(FolioService.getUniqueCategories().sort());
         });
 
-        var originatorEv;
-        $scope.openMenu = function($mdOpenMenu, ev) {
-          originatorEv = ev;
-          $mdOpenMenu(ev);
-        };
+    var originatorEv;
+    $scope.openMenu = function($mdOpenMenu, ev) {
+      originatorEv = ev;
+      $mdOpenMenu(ev);
+    };
+
+    function querySearch (query) {
+      var results = query ? $scope.categories.filter(createFilterFor(query)) : [];
+      return results;
+    }
+
+    function createFilterFor(query) {
+      var lowercaseQuery = angular.lowercase(query);
+
+      return function filterFn(category) {
+        console.log(category.indexOf(lowercaseQuery), ' . ');
+        console.log(category);
+        return category.indexOf(lowercaseQuery) === 0;
+      };
+
+    }
 
   }]);
 })(window.angular);
