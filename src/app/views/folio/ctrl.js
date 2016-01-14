8a3204fb222c92b785a0d8429e2658eb5b755bf1
Dave Collier <davetheflashguy@gmail.com>
2016-01-13 21:35:24 -0500

    Patching chips

--- src/app/views/folio/ctrl.js
+++ src/app/views/folio/ctrl.js
@@ -1,14 +1,5 @@
 (function (angular) {
   angular.module('folioApp').controller('FolioCtrl', ['$scope', '$timeout', '$q', 'FolioService', function($scope, $timeout, $q, FolioService){
-    $scope.readonly = false;
-    $scope.selectedItem = null;
-    $scope.searchText = null;
-    $scope.querySearch = querySearch;
-    $scope.categories = [];
-    $scope.selectedCategories = [];
-    $scope.numberChips = [];
-    $scope.numberChips2 = [];
-    $scope.numberBuffer = '';
 
     var promise = FolioService.getData();
         promise.then(function(data) {
@@ -17,6 +8,7 @@
           $scope.categories = FolioService.getUniqueCategories().sort();
           $scope.tags = FolioService.getUniqueTags().sort();
           $scope.years = FolioService.getUniqueYears().sort().reverse();
+          init();
         });
 
     var originatorEv;
@@ -26,6 +18,7 @@
     };
 
     function querySearch (query) {
+      console.log($scope.categories);
       var results = query ? $scope.categories.filter(createFilterFor(query)) : [];
       return results;
     }
@@ -34,11 +27,29 @@
       var lowercaseQuery = angular.lowercase(query);
 
       return function filterFn(category) {
-        console.log(category.indexOf(lowercaseQuery), ' . ');
-        console.log(category);
-        return category.indexOf(lowercaseQuery) === 0;
-      };
+        return (category._lowername.indexOf(lowercaseQuery) === 0);
+      }
 
+    }
+
+    function init() {
+      $scope.readonly = false;
+      $scope.selectedItem = null;
+      $scope.searchText = null;
+      $scope.querySearch = querySearch;
+      $scope.categories = getCategories();
+      $scope.selectedCategories = [];
+      $scope.numberChips = [];
+      $scope.numberChips2 = [];
+      $scope.numberBuffer = '';
+    }
+
+    function getCategories() {
+      var cats = FolioService.getUniqueCategories().sort();
+      return cats.map(function (cat) {
+        cat._lowername = cat.toLowerCase();
+        return cat;
+      });
     }
 
   }]);
