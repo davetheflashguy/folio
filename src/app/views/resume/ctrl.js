b81cb6695cd727e758dd47c13d5773dc9c6598ef
Dave Collier <davetheflashguy@gmail.com>
2016-03-28 21:00:52 -0400

    Resume refactor

--- src/app/views/resume/ctrl.js
+++ src/app/views/resume/ctrl.js
@@ -1,5 +1,11 @@
 (function (angular) {
-  angular.module('folioApp').controller('ResumeCtrl', ['$scope', function($scope){
+  angular.module('folioApp').controller('ResumeCtrl', ['$scope', 'ResumeService', function($scope, ResumeService){
     console.log("Resume Ctrl");
+    var promise = ResumeService.getData();
+        promise.then(function(data) {
+          $scope.intro = data.intro;
+          $scope.summary = data.summary;
+          $scope.experience = data.experience;
+        });
   }]);
 })(window.angular);
--- src/app/views/resume/service.js
+++ src/app/views/resume/service.js
@@ -1,5 +1,16 @@
 (function (angular) {
   angular.module('folioApp').service('ResumeService', ['$q', '$timeout', '$http', 'store', function ($q, $timeout, $http, store) {
+    var deferred = $q.defer();
+    $http.get('common/data/resume.json')
+    .success(function(data) {
+      deferred.resolve(data);
+    })
+    .error(function(data, status) {
+      console.error('Data error', status, data);
+    });
 
+    this.getData = function() {
+      return deferred.promise;
+    }
  }]);
 })(window.angular);
